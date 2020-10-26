2412f8b929e50c957cb930a633370bd649395466
WeMadeIt91 <70448825+WeMadeIt91@users.noreply.github.com>
2020-10-26 19:05:30 -0400

    Add files via upload

---
+++ Capstone Assignment Week 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is solely for the Coursera Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
